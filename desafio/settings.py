6fa0c72d12432fb56388e3d3a40097e53b162603
luysla <dyana.tavares1@gmail.com>
2019-08-07 16:32:25 -0400

    Configurando o projeto

--- desafio/settings.py
+++ desafio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@d!3f%3*32k8!bxi0oyc@beaqn^+b(#9rqma!8=crs6&*yg$c)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Fortaleza'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
